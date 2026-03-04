3f0cbbee583f080837d876c0c125821e7d16868a
Shriram Rajeshwar Nawathe <shrinawathe@gmail.com>
2026-03-04 13:18:37 +0530

    primitives

--- 2DataType/1primitves.js
+++ 2DataType/1primitves.js
@@ -2,21 +2,21 @@
 
 // name string boolean -(true false) null undefined
 let fname="John";
-console.log(fname);//John
+console.log("First Name: " + fname);//John
 console.log(typeof fname);//string
 
 let age=30;
-console.log(age);//30
+console.log("Age: " + age);//30
 console.log(typeof age);//number
 
 let isMarried=false;
-console.log(isMarried);//false
+console.log("Is Married: " + isMarried);//false
 console.log(typeof isMarried);//boolean
 
 let x=null;
-console.log(x);//null
+console.log("Value of x: " + x);//null
 console.log(typeof x);//object
 
 let y;
-console.log(y);//undefined
+console.log("Value of y: " + y);//undefined
 console.log(typeof y);//undefined
