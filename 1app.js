5e08f8762362378937bf99bfd65b7fd41e1a52d4
Shriram Rajeshwar Nawathe <shrinawathe@gmail.com>
2026-03-04 12:49:50 +0530

    name change

---
+++ 1app.js
@@ -0,0 +1,24 @@
+console.log("Hello World!");
+age=20;
+console.log(age);// 20   
+console.log(typeof age);//number    
+
+fname="Tony Stark";
+console.log(fname);//Tony Stark
+console.log(typeof fname);//string
+
+x=null;
+console.log(x);//null
+console.log(typeof x);//object
+
+var y;
+console.log(y);//undefined
+console.log(typeof y);//
+
+inFollow=true;
+console.log(inFollow);//true
+console.log(typeof inFollow);//boolean
+
+isF=false;
+console.log(isF);//false
+console.log(typeof isF);//boolean
