72f48cc42bcb45350d9efaef5f79fb9785c2226a
Shriram Rajeshwar Nawathe <shrinawathe@gmail.com>
2026-03-04 15:49:33 +0530

    objects basic done

--- 2DataType/2objects.js
+++ 2DataType/2objects.js
@@ -4,5 +4,27 @@
 // let obj{
 // key: value
 // };
+// var let
+// 
+const students={
+    fname:"Shri",
+    age:26,
+    marks:99,
+    ispass:"true",
+};
+console.log(students);// give object value
+console.log(typeof students);// give object value
 
+console.log(students.fname);
+console.log("[] use",students["fname"])
+console.log(typeof students.fname);
 
+//in const inside object value can be updated
+
+students.age+=2;//28
+students.age=30;
+console.log(students.age);
+
+// clg=> enter
+// console.log(students.ispass);
+console.log(students["ispass"]);
