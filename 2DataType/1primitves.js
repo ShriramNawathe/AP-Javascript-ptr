f7a32e187f73541e7693c2b438d2d8bf59d50a67
Shriram Rajeshwar Nawathe <shrinawathe@gmail.com>
2026-03-04 13:17:39 +0530

    primitive data types

--- 2DataType/1primitves.js
+++ 2DataType/1primitves.js
@@ -1,1 +1,22 @@
 // primituve data types
+
+// name string boolean -(true false) null undefined
+let fname="John";
+console.log(fname);//John
+console.log(typeof fname);//string
+
+let age=30;
+console.log(age);//30
+console.log(typeof age);//number
+
+let isMarried=false;
+console.log(isMarried);//false
+console.log(typeof isMarried);//boolean
+
+let x=null;
+console.log(x);//null
+console.log(typeof x);//object
+
+let y;
+console.log(y);//undefined
+console.log(typeof y);//undefined
