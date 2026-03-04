6488f1996fe11b2a5bc9b1f9e6771b953c0b2db1
Shriram Rajeshwar Nawathe <shrinawathe@gmail.com>
2026-03-04 17:18:41 +0530

    if else

---
+++ 4loops/if.js
@@ -0,0 +1,23 @@
+let age=25;
+if(age>18){
+ console.log("Vote yes");
+}
+
+
+let num=10;
+// if(num%2===0){
+if(num%2!==0){
+    console.log("even num")
+}else{
+    console.log("odd num")
+}
+
+// array
+let num1=[1,2,3,4,5,6,7,8,9,10];
+for(let i=0;i<num1.length;i++){
+   if(num1[i]%2===0){
+    console.log("even num ",num1[i]);
+}else{
+    console.log("odd num",num1[i]);
+};
+};
