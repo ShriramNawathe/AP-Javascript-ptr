d6ff86a426081d8f08dfd0893fb463fdc6c11f6d
Shriram Rajeshwar Nawathe <shrinawathe@gmail.com>
2026-03-04 16:41:46 +0530

    arith

---
+++ 3ArithmaticOperaters/1arithmetic.js
@@ -0,0 +1,31 @@
+let a=100;
+let b=80;
+console.log(a,b);
+console.log(`a=${a} and b=${b}`);
+
+console.log("a-b",a-b);
+console.log("a*b="+ a*b);
+console.log("a/b=",a/b);
+
+// modulas op-reminder
+let c=5;
+let d=4;
+console.log("a%b=",c%d); //4)5( = remender=1
+
+console.log("exponential c**d",c**d);//625
+
+let k=5
+k++;// k=k+1 or k+=1
+console.log("increment of k=",k);
+
+let kk=5
+--kk; // kk=kk-1 or kk-=1
+console.log("predecrementof kk=",kk)
+
+// console oprators
+console.log("operators");
+let x=5;
+let y=2;
+
+console.log(x*=4)//x=20
+console.log(x=x**y)//20**2==400
